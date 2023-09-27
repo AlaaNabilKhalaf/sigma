6cc9c454c2c18856aee49105286fe397cf74d23a
alaa nabil <alaakhalaf696@gmail.com>
2023-09-27 10:13:36 +0300

    Adding QR Reader Screen UI

--- lib/views/widgets/home_widgets/home_view_bogy.dart
+++ lib/views/widgets/home_widgets/home_view_bogy.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sigma/cubit/layout_cubit/layout_cubit.dart';
+import 'package:sigma/cubit/layout_cubit/layout_states.dart';
+import 'package:sigma/views/scanner_view.dart';
 import 'package:sigma/views/widgets/sheared_widgets/custom_button.dart';
 import 'package:sigma/views/widgets/sheared_widgets/custom_upper_container.dart';
 import 'package:sigma/views/widgets/sheared_widgets/some_icons.dart';
@@ -11,26 +16,34 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final lang = S.of(context);
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        const Expanded(
+    return BlocConsumer<LayoutCubit ,  LayoutStates>(builder: (context , state ){
+      final cubit = BlocProvider.of<LayoutCubit>(context);
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Expanded(
             child: CustomUpperContainer(
               langIcon: LanguageIcon(),
               navigatorIcon: NavigatorIcon(),
             ),
-        ),
-        Expanded(
-          flex: 3,
-          child: Column(
-            children: [
-              Image.asset("assets/images/4137379-removebg-preview 1.png"),
-              SizedBox(height: 10.h,),
-              CustomButtonChild(title: lang.QrScanner, fontSize:30 , width: 250.w, height: 56),
-            ],
           ),
-        )
-      ],
-    );
+          Expanded(
+            flex: 3,
+            child: Column(
+              children: [
+                Image.asset("assets/images/4137379-removebg-preview 1.png"),
+                SizedBox(height: 10.h,),
+                GestureDetector(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=> QrScanner(userId: cubit.userId!)));
+                    },
+                    child: CustomButtonChild(title: lang.QrScanner, fontSize:30 , width: 250.w, height: 56)),
+              ],
+            ),
+          )
+        ],
+      );
+    },
+        listener: (context , state ){});
   }
 }
